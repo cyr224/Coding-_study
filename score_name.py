5b9dbe7ea7d04ba016f4f5377f7e77696402a407
cyr224 <qoemal@naver.com>
2020-08-21 18:00:11 +0900

    Create score_name.py

---
+++ score_name.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+#m, k = map(list(int, input().split()))
+
+array = []
+for i in range(n):
+    input_data = input().split()
+    array.append((input_data[0], int(input_data[1])))
+
+array = sorted(array, key = lambda student : student[1])
+
+for student in array:
+    print(student[0], end = ' ')
